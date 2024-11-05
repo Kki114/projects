0b670c5bb0e7607636b30f6f2528f6a80cea24a9
Khari <khariw921@gmail.com>
2024-11-04 20:34:44 -0500

    Created venv & began experimenting

--- packet-generator/packet-generator.py
+++ packet-generator/packet-generator.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python
 
+from scapy.all import Ether, IP, ICMP, sr1
+
+data = b'Hello Router'
+
+pkt = IP(src="192.168.10.106", dst="192.168.10.1")/ICMP()/data
+
+resp = sr1(pkt)
+
+resp.show()
