9658ac1c06ffefab9b2ae20bf42f04bb345cbd50
Khari <khariw921@gmail.com>
2024-11-04 21:00:03 -0500

    Added brief description

--- packet-generator/packet-generator.py
+++ packet-generator/packet-generator.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python
 
-from scapy.all import Ether, IP, ICMP, sr1
+"""
+Created by Khari Walker
+
+Date of creation: 11/4/2024
+
+Description:
+It's largely just a fun project that I plan to flesh out into a 
+semi-independent program that asks the user questions about what packets
+they intend to send.
+"""
+
+from scapy.all import Ether, ARP, IP, ICMP, sr1
 
 data = b'Hello Router'
 
