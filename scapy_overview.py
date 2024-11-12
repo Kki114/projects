f166a5dd638e5509e3a91cbdc4b703e23ed7ba5e
Khari Walker <126723926+Kki114@users.noreply.github.com>
2024-11-12 16:25:47 -0500

    Bombal_Projects folder upload

---
+++ scapy_overview.py
@@ -0,0 +1,13 @@
+from scapy.all import *
+# Capture STP (Spanning Tree Protocol) frame
+pkt = sniff(filter="ether dst 01:80:c2:00:00:00", count=1)
+# View captured frame
+#pkt[0]
+# View captured frame - show nicely
+#pkt[0].show()
+# View 802.3 Ethernet
+#pkt[0][0].show()
+# View Logical-Link Control
+#pkt[0][1].show()
+# View STP
+pkt[0][1].show()
